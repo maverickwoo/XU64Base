c967319145557c9e0c33344fb75a22dfa0eac0ea
Maverick Woo <maverick@cs.cmu.edu>
2014-12-16 15:11:30 -0500

    pyenv support

---
+++ provision/public/root/etc/profile.d/pyenv.sh
@@ -0,0 +1,39 @@
+if [ -d "$HOME/.pyenv" ]; then
+    export PATH="$HOME/.pyenv/bin:$PATH";
+    eval "$(pyenv init -)";
+    eval "$(pyenv virtualenv-init -)";
+fi
+
+pyenv_init ()
+{
+    local PY27=2.7.9
+    local PY34=3.4.2
+    curl -L \
+         https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer \
+        | bash
+    source /etc/profile.d/pyenv.sh
+
+    sudo apt-get install -y \
+         libbz2-dev \
+         libreadline-dev \
+         libsqlite3-dev \
+         libssl-dev \
+         zlib1g-dev \
+         `#end`
+
+    pyenv install $PY27 &
+    pyenv install $PY34 &
+    wait
+
+    pyenv virtualenv $PY27 venv27
+    pyenv virtualenv $PY34 venv34
+
+    pyenv global $PY34              #I prefer python 3
+    pip install --user \
+        bpython \
+        cdiff \
+        pip-tools \
+        `#end`
+
+    pyenv versions
+}
