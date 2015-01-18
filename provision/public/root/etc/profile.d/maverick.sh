3c45effe240fffa612d5c2f960495a4b93beb0e6
Maverick Woo <maverick@cs.cmu.edu>
2015-01-17 21:00:23 -0500

    start moving my config to public

--- provision/public/root/etc/profile.d/maverick.sh
+++ provision/public/root/etc/profile.d/maverick.sh
@@ -1,3 +1,20 @@
+# things that I don't want to live without
+export LANG=en_US.utf-8         #I hate utf-8 in terminal, but it's everywhere
+export LC_COLLATE=C             #I want sorting by C
+stty -ixon <&2                  #enable C-s & avoid "stdin isn't a terminal"
+set -o ignoreeof                #ignore Ctrl-d
+set -o noclobber                #use >| to overwrite files
+shopt -s checkwinsize
+
+# share history across shells
+shopt -s histappend
+HISTCONTROL=ignoreboth
+HISTFILESIZE=9999999
+HISTSIZE=$HISTFILESIZE
+HISTTIMEFORMAT='%F %T '
+PROMPT_COMMAND="$PROMPT_COMMAND history -a; echo '#$(date +%s);' >> ~/.bash_history; history -n;"
+
+# common functions
 num_proc ()
 {
     local extra=${1:-0};
