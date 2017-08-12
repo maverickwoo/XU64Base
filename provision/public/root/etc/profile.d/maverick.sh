5f530c3ae3589a85d62c104385992447086837f9
Maverick Woo <maverick@cs.cmu.edu>
2017-08-12 13:11:05 -0400

    I prefer rougify for LESSOPEN

--- provision/public/root/etc/profile.d/maverick.sh
+++ provision/public/root/etc/profile.d/maverick.sh
@@ -18,22 +18,33 @@ HISTTIMEFORMAT='%F %T '
 PROMPT_COMMAND="$PROMPT_COMMAND history -a; echo '#$(date +%s);' >> ~/.bash_history; history -n;"
 
 # pager and other common stuff
+[ -r ~/bin/z.git/z.sh ] && . ~/bin/z.git/z.sh
 export EDITOR=vim
 export LESS=iMRSX               #-F seems to cause problem with lessopen
 export PAGER="less -$LESS"
 export VISUAL=vim               #get into the habit of using vim in terminal
-[ -r ~/bin/z.git/z.sh ] && . ~/bin/z.git/z.sh
-if [ -r ~/bin/lesspipe.git/lesspipe.sh ]; then
+if [ -x ~/bin/lesspipe.git/lesspipe.sh ]; then
     export LESSOPEN='| ~/bin/lesspipe.git/lesspipe.sh %s'
+fi
+if [ -x ~/bin/src-hilite-lesspipe.sh ]; then
+    export LESSOPEN='| ~/bin/src-hilite-lesspipe.sh %s'
+fi
+if command -v pygmentize > /dev/null; then
+    export LESSOPEN='| pygmentize -g %s'
+fi
+if command -v rougify > /dev/null; then
+    export LESSOPEN='| rougify highlight -t base16.dark "%s"'
 fi
 
 # common functions
-num_proc ()
+num_proc () # on linux, nproc will do
 {
     local extra=${1:-0};
     local ans=$(case "$OSTYPE" in
-                    linux*)  grep -cF processor /proc/cpuinfo;;
-                    darwin*) sysctl -n hw.logicalcpu;;
+                    linux*)  grep -cF processor /proc/cpuinfo
+                             ;;
+                    darwin*) sysctl -n hw.logicalcpu
+                             ;;
                 esac);
     echo $(($ans + $extra))
 }
